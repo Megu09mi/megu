bfb365f2d3f0ca99962f8ac1b1a045b91541351d
Megu09mi <136229766+Megu09mi@users.noreply.github.com>
2023-06-19 16:03:08 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,94 @@
+$('.about-slider').slick({
+  autoplay: true,//自動的に動き出すか。初期値はfalse。
+  infinite: true,//スライドをループさせるかどうか。初期値はtrue。
+  slidesToShow: 3,//スライドを画面に3枚見せる
+  slidesToScroll: 3,//1回のスクロールで3枚の写真を移動して見せる
+  prevArrow: '<div class="slick-prev"></div>',//矢印部分PreviewのHTMLを変更
+  nextArrow: '<div class="slick-next"></div>',//矢印部分NextのHTMLを変更
+  dots: true,//下部ドットナビゲーションの表示
+  responsive: [
+    {
+    breakpoint: 769,//モニターの横幅が769px以下の見せ方
+    settings: {
+      slidesToShow: 2,//スライドを画面に2枚見せる
+      slidesToScroll: 2,//1回のスクロールで2枚の写真を移動して見せる
+    }
+  },
+  {
+    breakpoint: 426,//モニターの横幅が426px以下の見せ方
+    settings: {
+      slidesToShow: 1,//スライドを画面に1枚見せる
+      slidesToScroll: 1,//1回のスクロールで1枚の写真を移動して見せる
+    }
+  }
+]
+});
+
+
+/*=================================================
+  スムーススクロール
+  ===================================================*/
+  $('a[href^="#"]').click(function () {
+    let href = $(this).attr("href");
+    let target = $(href == "#" || href == "" ? "html" : href);
+    let position = target.offset().top;
+    $("html, body").animate({ scrollTop: position }, 600, "swing");
+    return false; 
+  });
+
+
+  
+
+//ローディング画面
+
+$(function(){
+  $(window).on('load',function(){
+      $('.loader').delay(500).fadeOut(500);
+      $('.loader-bg').delay(800).fadeOut(700);
+  });
+  setTimeout(function(){
+      $('.loader-bg').fadeOut(500);
+  },5000);
+});
+
+//ハンバーガーメニュー
+$(function () {
+  $(".humbarger").on("click", function () {
+    $("header").toggleClass("open");
+  });
+
+  $("#mask").on("click", function () {
+    $("header").removeClass("open");
+  });
+   
+  $("nav a").on("click", function () {
+    $("header").removeClass("open");
+  });
+
+});
+
+ /*=================================================
+  スクロール時の画像フェード表示
+  ===================================================*/
+  // スクロール時のイベント
+  $(window).scroll(function () {
+    // fadeinクラスに対して順に処理を行う
+    $(".fadein").each(function () {
+      // スクロールした距離
+      let scroll = $(window).scrollTop();
+      // fadeinクラスの要素までの距離
+      let target = $(this).offset().top;
+      // 画面の高さ
+      let windowHeight = $(window).height();
+      // fadeinクラスの要素が画面下にきてから200px通過した
+      // したタイミングで要素を表示
+      if (scroll > target - windowHeight + 200) {
+        $(this).css("opacity", "1");
+        $(this).css("transform", "translateY(0)");
+      }
+    });
+  });
+
+
+
+
